14fc62d71e00ee3ac0c06665877b9f6efd06f558
Zakhar <79360682+Zaha-Proger@users.noreply.github.com>
2026-05-05 13:13:16 +0300

    Update app_logger.py

--- app_logger.py
+++ app_logger.py
@@ -7,7 +7,6 @@ LOG_FILE = os.path.join(LOG_DIR, "app.log")
 def setup_logger():
     if not os.path.exists(LOG_DIR):
         os.makedirs(LOG_DIR)
-
     logging.basicConfig(
         filename=LOG_FILE,
         level=logging.INFO,
